6228e5de16fc04ec0f1e9b6e9b84a24a0dd93c18
BaeGakKyeong <bggcarl@gmail.com>
2026-02-09 12:28:42 +0900

    오버라이딩 예제 2

--- src/chp07ex06/Bus.java
+++ src/chp07ex06/Bus.java
@@ -2,6 +2,7 @@ package chp07ex06;
 
 public class Bus extends Car {
 	public void run() {
-		System.out.println("차가 달리면서 다음 정거장을 안내합니다.");
+		super.run();
+		System.out.println("다음 정거장을 안내합니다.");
 	}
 }
