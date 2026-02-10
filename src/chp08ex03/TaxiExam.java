96823781b124cba24a3169f46dee077145feb570
BaeGakKyeong <bggcarl@gmail.com>
2026-02-10 11:04:40 +0900

    인터페이스의 default 메서드 예제

---
+++ src/chp08ex03/TaxiExam.java
@@ -0,0 +1,13 @@
+package chp08ex03;
+
+public class TaxiExam {
+
+	public static void main(String[] args) {
+		Taxi taxi = new Taxi();
+		
+		taxi.start();
+		taxi.afterMidnight();
+		taxi.stop(10);
+	}
+
+}
