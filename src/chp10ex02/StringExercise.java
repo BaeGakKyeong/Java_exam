1843683cc92d3824086c39c3bd70559dfc5bc1f2
BaeGakKyeong <bggcarl@gmail.com>
2026-02-12 17:35:33 +0900

    lesson53 풀이

---
+++ src/chp10ex02/StringExercise.java
@@ -0,0 +1,20 @@
+package chp10ex02;
+
+public class StringExercise {
+
+	String getMiddle(String word) {
+		if(word.length() % 2 == 1) {
+			return word.substring(word.length() / 2, word.length() / 2 + 1);
+		}
+		else {
+			return word.substring(word.length() / 2 - 1, word.length() / 2 + 1);
+		}
+	}
+	public static void main(String[] args) {
+		StringExercise se = new StringExercise();
+		
+		System.out.println(se.getMiddle("power"));
+		System.out.println(se.getMiddle("test"));
+	}
+
+}
