261d69650ec81d1806937a64c0e4d82ea3b40c0c
BaeGakKyeong <bggcarl@gmail.com>
2026-02-13 16:14:33 +0900

    Lesson56 다른 풀이

--- src/chp10ex05/EvenOrOdd.java
+++ src/chp10ex05/EvenOrOdd.java
@@ -3,12 +3,14 @@ package chp10ex05;
 public class EvenOrOdd {
 
 	String evenOrOdd(int num) {
-		if(num % 2 == 0) {
-			return "Even";
-		}
-		else {
-			return "Odd";
-		}
+//		if(num % 2 == 0) {
+//			return "Even";
+//		}
+//		else {
+//			return "Odd";
+//		}
+//다음과 같이 코드를 한줄로 줄일 수도 있다.
+		return (num % 2 == 0) ? "Even" : "Odd";
 	}
 	public static void main(String[] args) {
 		EvenOrOdd evenOrOdd = new EvenOrOdd();	//메서드명과 참조형 변수명이 같지만, 컴파일 오류 없음.
