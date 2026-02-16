150afb192027bfc227e2d602be842a631144022c
BaeGakKyeong <bggcarl@gmail.com>
2026-02-16 14:05:20 +0900

    Lesson57 답지 참고 후 반복문 수정

--- src/chp10ex06/AsciiToInt.java
+++ src/chp10ex06/AsciiToInt.java
@@ -3,21 +3,10 @@ package chp10ex06;
 public class AsciiToInt {
 	public int ascii(String str, int i) {
 		int result = 0;
-		int asciiNum;
-			
 			
 		for(; i < str.length(); i++) {
-			asciiNum = str.charAt(i) - 48;
-			result += asciiNum;
-			
-			if(i == str.length() - 1) {
-				continue;
-			}
-			
-			result *= 10;
+			result = result * 10 + str.charAt(i) - '0';
 		}
-	
-			
 		
 		return result;
 	}
