be1044f75bdb180f86687a2a4e808da22142b1e0
BaeGakKyeong <bggcarl@gmail.com>
2026-02-20 12:34:20 +0900

    Lesson58 문제풀이

---
+++ src/chp10ex07/FindKim.java
@@ -0,0 +1,25 @@
+package chp10ex07;
+
+public class FindKim {
+
+	public String findKim(String[] seoul) {
+		int x = 0;
+		
+		for(int i = 0; i < seoul.length; i++) {
+			if(seoul[i] == "Kim") {
+				x = i;
+				break;
+			}
+		}
+		
+		return "김 서방은 " + x + "에 있다";
+	}
+	
+	public static void main(String[] args) {
+		FindKim kim = new FindKim();
+		
+		String[] names = {"Queen", "Tod", "Kim"};
+		System.out.println(kim.findKim(names));
+	}
+
+}
